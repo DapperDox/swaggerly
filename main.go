d292ae1d37acf5657ddb1cc28c6cfd5f589eee71
frinka <49955850+frinka@users.noreply.github.com>
2019-05-10 12:41:09 -0500

    Increment version (Dapperdox/dapperdox is at 1.2.2)

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ import (
 	"github.com/justinas/nosurf"
 )
 
-var VERSION string = "1.2.1"
+var VERSION string = "1.2.3"
 var tlsEnabled bool
 
 // ---------------------------------------------------------------------------
